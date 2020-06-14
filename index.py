7a3d162d992862f5ec12108a4e11c0e6e60c2ba4
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-14 11:46:32 -0500

    Update index.py

--- index.py
+++ index.py
@@ -3,17 +3,19 @@ import datetime
 
 def handler(event, context):
     
-    n = int(1000)
+    n = int(9)
     if event is not None:
         n = int(event["queryStringParameters"]["numfib"])
-    lista = []
-    cadena = ''
-    a, b = 0,1
-    while a < n:
-        #print(a, end=' ')
-        cadena = cadena + str(a) + ' '
-        a, b = b, a+b
-    lista.append(cadena)
+        lista = []
+        cadena = ''
+        a, b = 0,1
+        while a < n:
+            #print(a, end=' ')
+            cadena = cadena + str(a) + ' '
+            a, b = b, a+b
+        lista.append(cadena)
+    else:
+        lista.append(n)
     
     data = {
         'result':lista,
