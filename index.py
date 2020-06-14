0c4664db0cab61a0be9a6e8979050e1990cd0909
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-13 20:28:56 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,20 @@
 import json
 import datetime
 
-
 def handler(event, context):
+    
+    n = int(1000)
+    if event is not None:
+        n = int(event["queryStringParameters"]["numfib"])
+    lista = []
+    cadena = ''
+    a, b = 0,1
+    while a < n:
+        #print(a, end=' ')
+        cadena = cadena + str(a) + ' '
+        a, b = b, a+b
+    lista.append(cadena)
+    
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
