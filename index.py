00154ec436bde6aadca7bfb9753d65f17ff0ef7e
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-16 16:01:17 -0500

    Update index.py

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ def handler(event, context):
         'numero':n,
         'fibonacci':lista,
         'triangular':triangular,
-        'output':'Hola'
+        'output':'Hola',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
