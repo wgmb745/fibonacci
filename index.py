d1096953ea615f51b69e4045d08115014c14f13c
wgmb745 <66256132+wgmb745@users.noreply.github.com>
2020-06-16 21:05:02 -0500

    Update index.py

--- index.py
+++ index.py
@@ -21,7 +21,7 @@ def handler(event, context):
         'numero':n,
         'fibonacci':lista,
         'triangular':triangular,
-        'output': 'Hello World',
+        'output': 'FIN',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
